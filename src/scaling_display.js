b2dc36a6c37217ad5fc0eca29092585829852e86
Marcin Wołkowicz <mwolkowicz17@gmail.com>
2021-10-22 08:27:31 +0200

    display height begin of work

--- src/scaling_display.js
+++ src/scaling_display.js
@@ -1,8 +1,12 @@
 import { display } from "./display.js";
 
 export function scalingDisplay(stringLenght){
-if(stringLenght<700){
-    display.style.height="320px"
+if(stringLenght<600){
+    display.style.height="320px";
     console.log("new height");
+}
+if(stringLenght>700 && stringLenght<1100){
+   display.style.height="420px";
+   console.log("new height2");
 }
 }
