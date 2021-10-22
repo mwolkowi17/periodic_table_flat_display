4bb5f5851d7e3290690be5636f9e19fbfcbb07b9
Marcin Wołkowicz <mwolkowicz17@gmail.com>
2021-10-22 19:50:21 +0200

    display high nearly ready

--- src/scaling_display.js
+++ src/scaling_display.js
@@ -2,13 +2,19 @@ import { display,objectdisplay } from "./display.js";
 import { display_tween } from "./index.js";
 
 export function scalingDisplay(stringLenght){
-if(stringLenght>700){
+if(stringLenght>600){
     display.style.height=(stringLenght/1.7).toString()+'px';
     console.log("new height");
     display_tween(-500, 0, -1000, 2000)
     //objectdisplay.position.set(-500, 0, -1500);
-}else{
-    display.style.height = "350px";
+    if(stringLenght>1200){
+        display.style.height=(stringLenght/2).toString()+'px';
+        console.log('high');
+    }
+}
+
+else{
+    display.style.height = "360px";
     display_tween(-500, 100, -1000, 2000)
 }
    
